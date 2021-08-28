60382effea0f04c7196b90077a9684a00b80dcc6
mrdm026M <mrdm026@gmail.com>
2021-08-28 06:03:26 +0530

    header basic layout done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <header className="header__section">
+      <div className="website__heading">
+        <h1>React | TO-DO📄</h1>
+      </div>
+    </header>
+  );
+};

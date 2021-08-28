cd6d2d4fa76e03e4dbe9a0291c6fa1c60ae82ab1
mrdm026M <mrdm026@gmail.com>
2021-08-28 06:02:32 +0530

    custom breakpoints added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    sm: "440px",
+    // => @media (min-width: 440px) { ... }
+
+    md: "547px",
+    // => @media (min-width: 547px) { ... }
+
+    lg: "768px",
+    // => @media (min-width: 768px) { ... }
+
+    xl: "1024px",
+    // => @media (min-width: 1024px) { ... }
+
+    xxl: "1280px",
+    // => @media (min-width: 1280px) { ... }
     extend: {},
   },
   variants: {

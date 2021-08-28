72c116e06473c2204c8c34c8822eb6fcb6aa5861
mrdm026M <mrdm026@gmail.com>
2021-08-29 00:27:48 +0530

    ui done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,9 +3,10 @@ import "./Header.scss";
 
 export const Header = () => {
   return (
-    <header className="header__section">
-      <div className="website__heading">
-        <h1>React | TO-DO📄</h1>
+    <header className="p-4 header__section">
+      <div className="m-4 text-center website__heading">
+        <h1 className="mb-2 text-3xl md:text-4xl">React | To-Do 📄</h1>
+        <hr className="m-auto bg-white border-none h-0.5 w-40 md:w-80" />
       </div>
     </header>
   );
